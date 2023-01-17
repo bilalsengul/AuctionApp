9c36f4d5b0e53ff6c8557d11d874fc8642b36919
Bilal Şengül <bilall.sengull@gmail.com>
2023-01-17 05:15:56 +0300

    Initial commit backend

---
+++ src/main/java/com/sengul/biddingapinew/BiddingApiNewApplication.java
@@ -0,0 +1,13 @@
+package com.sengul.biddingapinew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingApiNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiddingApiNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sengul/biddingapinew/BiddingApiNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sengul.biddingapinew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingApiNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
